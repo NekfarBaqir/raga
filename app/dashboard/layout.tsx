ee3da8a692bb7249ec3c9917b189ad504c0b2307
rovrbaqir <m.nekfar.baqir.786@gmail.com>
2025-09-04 09:43:59 +0430

    RP-3: dashboard init

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   <div className="w-full flex flex-col justify-start ites">
+    {/* sidebar */}
+    {/* header */}
+    {children}
+   </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      Welcome to the dashboard
+    </div>
+  )
+}
+
+export default page
